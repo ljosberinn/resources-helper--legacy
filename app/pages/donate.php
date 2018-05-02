5d4dab9af076be33f899b8d0a7e09a2b8985f1b3
Gerrit Alex <admin@gerritalex.de>
2018-05-02 17:02:51 +0200

    small language improvement

--- app/pages/donate.php
+++ app/pages/donate.php
@@ -8,10 +8,8 @@
   <p>This page was developed from scratch by a single person.<br />
   If you like what you see, please consider contributing any amount you can afford.</p>
 
-  <p><a href="https://paypal.me/GerritAlex" target="_blank" rel="noopener noreferrer">Donate via PayPal</a></p>
+  <p><a href="https://paypal.me/GerritAlex" target="_blank" rel="noopener noreferrer">Donate via PayPal</a> or ingame to <kbd>Chevron</kbd></p>
 
-  <p>Donate ingame to <kbd>Chevron<kbd></p>
-
-  Thank you.
+  Thank you!
 
 </div>
