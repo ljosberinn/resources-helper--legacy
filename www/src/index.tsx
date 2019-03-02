f68020047f96ea95af6a65925043e85a9da9447b
ljosberinn <admin@gerritalex.de>
2019-03-02 17:34:20 +0100

    integration of TypeScript-React-Starter (TS create-react-app)

---
+++ www/src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+//import registerServiceWorker from './registerServiceWorker';
+import ApiKey from "./components/ApiKey";
+
+ReactDOM.render(
+  <ApiKey/>,
+  document.getElementById("root") as HTMLElement
+);
+
+//registerServiceWorker();
