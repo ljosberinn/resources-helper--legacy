e77df83520e41c89a3507695a5d26e30d5b42694
ljosberinn <admin@gerritalex.de>
2019-03-30 12:45:07 +0100

    renaming data -> api

--- src/developmentSettings.tsx
+++ src/developmentSettings.tsx
@@ -1,7 +1,7 @@
 export const DEV_SETTINGS = {
   isLive: false,
   uri: {
-    development: 'https://localhost/rhelper4/public/data',
+    development: 'https://localhost/rhelper4/public/api',
     live: '',
   },
   version: '4.0.0',
