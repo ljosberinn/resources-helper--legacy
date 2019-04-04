881537424916defd4fc54718235e6d2e1c38bb66
ljosberinn <admin@gerritalex.de>
2019-04-04 21:53:54 +0200

    removing minification for API

--- public/_boot.php
+++ public/_boot.php
@@ -19,19 +19,3 @@ spl_autoload_register(function($className) {
 $dotenv = Dotenv::create(__DIR__);
 $dotenv->load();
 $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS']);
-
-ob_start(function($buffer) {
-    $search = [
-        '/\>[^\S ]+/s',      // strip whitespaces after tags, except space
-        '/[^\S ]+\</s',      // strip whitespaces before tags, except space
-        '/(\s)+/s',          // shorten multiple whitespace sequences
-        '/<!--(.|\s)*?-->/', // Remove HTML comments
-    ];
-    $replace = [
-        '>',
-        '<',
-        '\\1',
-        '',
-    ];
-    return preg_replace($search, $replace, $buffer);
-});
